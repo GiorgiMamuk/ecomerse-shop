b4d0b516234a5e19f434cdbbe56ad5fdf03812f1
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-07-14 19:47:43 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+function App() {
+  
+  
+  return (
+    <div className="App flex flex-col justify-between h-[100vh]">
+      
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {}
+
+function Main(props: Props) {
+    const {} = props
+
+    return (
+        <>
+        
+        
+        </>
+    )
+}
+
+export default Main
