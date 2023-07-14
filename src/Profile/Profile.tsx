a7cc7a9e0026090a1366b8ca325d53100fe54590
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-07-14 21:24:29 +0400

    Profile component created

---
+++ src/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Profile(props: Props) {
+    const {} = props
+
+    return (
+        <>
+        
+        profile</>
+    )
+}
+
+export default Profile
